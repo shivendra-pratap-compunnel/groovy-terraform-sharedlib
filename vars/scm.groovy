f55e1a6c257283303ab1482ee5b05ebfad3a79ac
Shivendra <157563011+shivendra-pratap-compunnel@users.noreply.github.com>
2024-03-19 18:26:44 +0530

    Update scm.groovy

--- vars/scm.groovy
+++ vars/scm.groovy
@@ -2,7 +2,7 @@
 void call(Map scm = [:]) {
     // TODO simplify with elvis operator
     if (scm.credentials == null) {
-        scm.credentials = 'terrafrom-config'
+        scm.credentials = 'CDTeraformModules'
         echo scm.credentials
     } else {
         echo scm.credentials
