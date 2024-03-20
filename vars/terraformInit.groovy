53fcca626d259d708a8fb8b456b28df29930a5e5
Shivendra <157563011+shivendra-pratap-compunnel@users.noreply.github.com>
2024-03-20 10:53:56 +0530

    Update terraformInit.groovy

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -1,6 +1,11 @@
-void terraform.init() {
-        echo 'Generating provider.tf...'
-        sh '''
-            terraform init -no-color
-        '''
+void call() {
+    echo 'Generating provider.tf...'
+    // Define the command to execute
+    def command = "terraform init -no-color"
+    
+    // Execute the command
+    def proc = command.execute()
+    
+    // Wait for the command to finish and capture the output
+    proc.waitForProcessOutput(System.out, System.err)
 }
