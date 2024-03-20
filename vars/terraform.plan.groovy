5bda377a70127e6faeedb9ab4fe3548572100a87
Shivendra <157563011+shivendra-pratap-compunnel@users.noreply.github.com>
2024-03-20 11:01:29 +0530

    Update terraform.plan.groovy

--- vars/terraform.plan.groovy
+++ vars/terraform.plan.groovy
@@ -1,7 +1,14 @@
-void terraform.plan() {
+void call() {
     sh '''
         echo "----- Creating plan to validate -----"
         terraform plan -no-color -out=tfplan -input=false
         terraform show -no-color -json tfplan > tfplan.json
     '''
 }
+
+
+// void call() {
+//      echo "----- Creating plan to validate -----"
+//     sh 'terraform plan -no-color -out=tfplan -input=false'
+//     sh  'terraform show -no-color -json tfplan > tfplan.json'
+// }
